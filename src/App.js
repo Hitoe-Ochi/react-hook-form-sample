7f43279a42e4f8b10680deaae162ab8bbc03145c
Hitoe-Ochi <han.xutoe@gmail.com>
2022-01-25 20:58:56 +0900

    react-hook-formでのバリデーション機能のサンプル実装

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TextField } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
+import { Box } from "@mui/material";
+import { Button } from "@mui/material";
 
-function App() {
+const defaultValues = {
+  TextField: "",
+  comment: "hi hitoe",
+};
+
+export default function App() {
+  const {
+    handleSubmit,
+    control,
+    setValue,
+    formState: { errors },
+    getValues
+  } = useForm({
+    mode: "onChange",
+    defaultValues,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Box sx={{margin: 2}}>
+        <Button variant="contained" onClick={() => setValue("TextField", "User A")}>
+          {" "}
+          User A
+        </Button>
+        <Button variant="contained" onClick={() => setValue("TextField", "User B")}>
+          User B
+        </Button>
+      </Box>
+      <Box sx={{padding: 2}}>
+        <form onSubmit={handleSubmit((data) => console.log(data))}>
+          <Box sx={{ display: "flex", textAlign: "center", margin: 2 }}>
+            <label>MUI TextField</label>
+            <Controller
+              render={({ field }) => (
+                <TextField
+                  onChange={(e) =>
+                    setValue("TextField", e.target.value, {
+                      shouldValidate: true,
+                    })
+                  }
+                  error={Boolean(errors.TextField)}
+                  helperText={errors.TextField && "error"}
+                  {...field}
+                />
+              )}
+              name="TextField"
+              control={control}
+              rules={{ required: true, maxLength: 10, minLength: 3 }}
+            />
+          </Box>
+
+          <Box sx={{ display: "flex", textAlign: "center", margin: 2 }}>
+            <label>MUI TextField 02</label>
+            <Controller
+              render={({ field }) => (
+                <TextField
+                  onChange={(e) =>
+                    setValue("comment", e.target.value, {
+                      shouldValidate: true,
+                    })
+                  }
+                  error={Boolean(errors.comment)}
+                  helperText={errors.comment && "error"}
+                  {...field}
+                />
+              )}
+              name="comment"
+              control={control}
+              rules={{ required: true, maxLength: 10, minLength: 3 }}
+            />
+          </Box>
+
+          {/* <input type="submit" /> */}
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </form>
+
+        <div>
+          {getValues("TextField")}
+        </div>
+      </Box>
+    </>
   );
 }
-
-export default App;
